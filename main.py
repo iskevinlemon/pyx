d542f2114d747ce2bb9f25f6fbb02c542e90a0af
Kevin <126497052+kevin-lem0n@users.noreply.github.com>
2023-11-18 18:23:49 +0800

    Added more sample variable to main.py

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ port = 3000
 
 # Specify the data as dictionary
 data = {
+    # Custom defined data
+    "page_title": "Welcome to pyx",
     "fruit": "apple",
     "car": "toyota",
-    "error_message": "The page you have requested cannot be found"
+
+    # Error pages
+    "not_found_error_message": "The page you have requested cannot be found",
+    "forbidden_error_message": "You do not have permission to access this page"
 }
 
 # Start the server with the specified port and pass in the data dictionary
